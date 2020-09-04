199ac0e2fe7fa0c9a74746ec882440c8a7024206
unknown <armando.zegarra@ucsp.edu.pe>
2020-09-03 23:47:05 -0500

    Updates

---
+++ precache-manifest.84febf3aa7d3596d8dba3e29873dda63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bdcdf6fd0dca067e323107244d9e8a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "be5c4b6d19a55bf14a50",
+    "url": "/monsters-rolodex/static/css/main.96ed5d55.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5c4b6d19a55bf14a50",
+    "url": "/monsters-rolodex/static/js/main.60f19890.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
